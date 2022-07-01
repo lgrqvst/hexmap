e2165ac4658f5fae22f3f84998d47fb4c120bab9
Johan Lagerqvist <johan.lagerqvist@linecorp.com>
2022-07-02 08:42:25 +0900

    Hide some of the grid effects, remove flicker

--- src/canvas/drawGrid.ts
+++ src/canvas/drawGrid.ts
@@ -13,47 +13,51 @@ export const drawGrid = (
     const radius = heightRow
 
     row.forEach((p) => {
-      ctx.beginPath()
-      ctx.arc(p.x, p.y, 2, 0, 2 * Math.PI)
-      ctx.fillStyle = 'rgba(255, 255, 255, 1)'
-      ctx.fill()
-      ctx.closePath()
+      // // Center dot
+      // ctx.beginPath()
+      // ctx.arc(p.x, p.y, 2, 0, 2 * Math.PI)
+      // ctx.fillStyle = 'rgba(255, 255, 255, 1)'
+      // ctx.fill()
+      // ctx.closePath()
 
-      ctx.beginPath()
-      ctx.arc(p.x, p.y, 10, 0, 2 * Math.PI)
-      ctx.strokeStyle = 'rgba(255, 255, 255, 0.3)'
-      ctx.stroke()
-      ctx.closePath()
+      // // Center small circle
+      // ctx.beginPath()
+      // ctx.arc(p.x, p.y, 10, 0, 2 * Math.PI)
+      // ctx.strokeStyle = 'rgba(255, 255, 255, 0.3)'
+      // ctx.stroke()
+      // ctx.closePath()
 
-      ctx.beginPath()
-      ctx.arc(p.x, p.y, heightRow, 0, Math.PI * 2)
-      ctx.strokeStyle = 'rgba(255, 255, 255, 0.5)'
-      ctx.stroke()
-      ctx.closePath()
+      // // heightRow circle
+      // ctx.beginPath()
+      // ctx.arc(p.x, p.y, heightRow, 0, Math.PI * 2)
+      // ctx.strokeStyle = 'rgba(255, 255, 255, 0.5)'
+      // ctx.stroke()
+      // ctx.closePath()
 
-      ctx.beginPath()
-      ctx.arc(p.x, p.y, unit, 0, Math.PI * 2)
-      ctx.strokeStyle = 'rgba(255, 255, 255, 0.25)'
-      ctx.stroke()
-      ctx.closePath()
+      // // unit circle
+      // ctx.beginPath()
+      // ctx.arc(p.x, p.y, unit, 0, Math.PI * 2)
+      // ctx.strokeStyle = 'rgba(255, 255, 255, 0.25)'
+      // ctx.stroke()
+      // ctx.closePath()
 
-      // First hex at size unit
-      ctx.save()
-      ctx.translate(p.x, p.y)
-      ctx.beginPath()
-      ctx.moveTo(p.radialPoints[5].x * unit, p.radialPoints[5].y * unit)
-      p.radialPoints.forEach((rp) => {
-        ctx.lineTo(rp.x * unit, rp.y * unit)
-      })
-      // ctx.strokeStyle = 'rgba(255, 0, 0, 1)'
-      ctx.strokeStyle = 'rgba(255, 255, 255, 0.25)'
-      ctx.lineWidth = 3
-      ctx.stroke()
-      // ctx.fillStyle = `rgba(255, 0, 0, ${random() * 0.1})`
-      ctx.fillStyle = `rgba(255, 0, 0, 0.05)`
-      ctx.fill()
-      ctx.closePath()
-      ctx.restore()
+      // // First hex at size unit
+      // ctx.save()
+      // ctx.translate(p.x, p.y)
+      // ctx.beginPath()
+      // ctx.moveTo(p.radialPoints[5].x * unit, p.radialPoints[5].y * unit)
+      // p.radialPoints.forEach((rp) => {
+      //   ctx.lineTo(rp.x * unit, rp.y * unit)
+      // })
+      // // ctx.strokeStyle = 'rgba(255, 0, 0, 1)'
+      // ctx.strokeStyle = 'rgba(255, 255, 255, 0.25)'
+      // ctx.lineWidth = 3
+      // ctx.stroke()
+      // // ctx.fillStyle = `rgba(255, 0, 0, ${random() * 0.1})`
+      // ctx.fillStyle = `rgba(255, 0, 0, 0.05)`
+      // ctx.fill()
+      // ctx.closePath()
+      // ctx.restore()
 
       // Second hex at size radius
       ctx.save()
@@ -63,27 +67,27 @@ export const drawGrid = (
       p.radialPoints.forEach((rp) => {
         ctx.lineTo(rp.x * radius, rp.y * radius)
       })
-      ctx.strokeStyle = 'rgba(255, 0, 0, 0.75)'
+      ctx.strokeStyle = 'rgba(255, 0, 0, 0.5)'
       ctx.lineWidth = 3
       ctx.stroke()
-      ctx.fillStyle = `rgba(255, 0, 0, ${random() * 0.25})`
+      ctx.fillStyle = `rgba(255, 0, 0, 0.05)`
       ctx.fill()
       ctx.closePath()
       ctx.restore()
 
-      // Hex spokes
-      ctx.save()
-      ctx.translate(p.x, p.y)
-      ctx.beginPath()
-      p.radialPoints.forEach((rp) => {
-        ctx.moveTo(rp.x * unit, rp.y * unit)
-        ctx.lineTo(rp.x * radius, rp.y * radius)
-      })
-      ctx.strokeStyle = 'rgba(255, 255, 255, 0.25)'
-      ctx.lineWidth = 3
-      ctx.stroke()
-      ctx.restore()
-      ctx.closePath()
+      // // Hex spokes
+      // ctx.save()
+      // ctx.translate(p.x, p.y)
+      // ctx.beginPath()
+      // p.radialPoints.forEach((rp) => {
+      //   ctx.moveTo(rp.x * unit, rp.y * unit)
+      //   ctx.lineTo(rp.x * radius, rp.y * radius)
+      // })
+      // ctx.strokeStyle = 'rgba(255, 255, 255, 0.25)'
+      // ctx.lineWidth = 3
+      // ctx.stroke()
+      // ctx.restore()
+      // ctx.closePath()
     })
   })
 }
